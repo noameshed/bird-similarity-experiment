4b3130b041999002c0f03f89eecdebff43be6d2d
Noam Eshed <noam_eshed@yahoo.com>
2020-04-04 21:25:51 -0400

    fixed CUB ResNet indexing

--- feature_extraction.py
+++ feature_extraction.py
@@ -12,9 +12,9 @@ import os
 from transfer_model import Transfer
 
 BATCH_SIZE = 1
-ROOT = "D:/noam_/Cornell/CS7999/iNaturalist/"
-TEST_DATA_PATH = os.path.join(ROOT, "experiment classes/Aves/")
-SAVE_DATA_PATH = os.path.join(ROOT, "features/resnet_block1/")
+# ROOT = "D:/noam_/Cornell/CS7999/iNaturalist/"
+# TEST_DATA_PATH = os.path.join(ROOT, "experiment classes/Aves/")
+# SAVE_DATA_PATH = os.path.join(ROOT, "features/resnet_block1/")
 TRANSFORM_IMG = transforms.Compose([
         transforms.Resize(224),
         transforms.CenterCrop(224),
@@ -42,35 +42,46 @@ class ImageFolderWithPaths(torchvision.datasets.ImageFolder):
         return tuple_with_path
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     """
     # Print AlexNet network summary
     print('ALEXNET')
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     alex = models.alexnet(pretrained=True).to(device)
     summary(alex,(3,244,244))
     print(nn.Sequential(*list(alex.children())))
-
+    """
     # Print VGG16 network summary
     print('VGG16')
     vgg = models.vgg16(pretrained=True).to(device)
     summary(vgg,(3,244,244))
     print(nn.Sequential(*list(vgg.children())))
-    """
+    
 
     # Print VGG16 network summary
     print('ResNet18')
     CUB_path = 'model_train_sgd-lr_1e-01-gamma_1e-01-epoch_100-stage_2-decay_1e-03-bs_64-gpu_2.pth'
     # model_state_dict = torch.load(CUB_path, map_location='cuda:0')
     resnet = Transfer(num_channel=32, num_class=100, resnet=True)
-    model_state = torch.load(CUB_path, map_location='cuda:0')
+    model_state = torch.load(CUB_path, map_location=device)  #'cuda:0' for desktop
     resnet.load_state_dict(model_state)
 
     # summary(model,(3,244,244))
-    print(nn.Sequential(*list(resnet.children())))
+    print(nn.Sequential(*list(resnet.children()))[0].layers[:3])
     
+
+    TEST_DATA_PATH = os.getcwd() + "/images/Aves/"
+    SAVE_DATA_PATH = os.getcwd() + "/features/resnet18_block_5/"
+
+    test_data = ImageFolderWithPaths(root=TEST_DATA_PATH, transform=TRANSFORM_IMG)
+    test_data_loader  = data.DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=4) 
+
+
     # Extract the partial model up to the point of interest
-    model_ft = models.vgg16(pretrained=True)
-    partial_model = nn.Sequential(*list(model_ft.children())[0][:3])
+    # model_ft = models.vgg16(pretrained=True)
+    model_ft = resnet
+
+    # partial_model = nn.Sequential(*list(model_ft.children())[0][:3])    
+    partial_model = nn.Sequential(*list(resnet.children()))[0].layers[:8]       # Slightly different structure for ResNet
     print('Partial model:\n',partial_model)
     
     # Set up model to not update parameters
